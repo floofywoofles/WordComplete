4dec9f2f62fc14c75469941cb571331f93c5e0b6
Kevin Knapp <111673584+floofywoofles@users.noreply.github.com>
2025-09-24 17:14:18 -0400

    Fixed the results to be closer to the actual word

--- index.ts
+++ index.ts
@@ -1,4 +1,18 @@
 import { levenshteinDistance, getWordDistances, bubbleSort } from "./src/autocomplete";
 
-const distances = await getWordDistances("Hello")
-console.log(bubbleSort(distances))
+const word = "word"
+const distances = bubbleSort(await getWordDistances(word))
+
+let out = [];
+
+for (let i = 0; i < distances.length; ++i) {
+    if (distances[i]!.word.toLowerCase().startsWith(word.toLowerCase())) {
+        out.push(distances[i])
+    }
+}
+
+if (!out.length) {
+    out = distances.slice(0, 10);
+}
+
+console.log(out);
